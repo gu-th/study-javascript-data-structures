5ff519842494f50feb709e02faff344752013bdc
gu_th <1842987765@qq.com>
2023-01-12 15:04:13 +0800

    update: prim算法

--- Graph/Prim.js
+++ Graph/Prim.js
@@ -2,7 +2,7 @@
  * @Author       : guth
  * @Date         : 2023-01-10 09:45:40
  * @LastEditors  : guth
- * @LastEditTime : 2023-01-10 15:04:49
+ * @LastEditTime : 2023-01-11 16:58:01
  * @FilePath     : /study-javascript-data-structures/Graph/Prim.js
  * @Description  : 一种求解加权无向连通图的MST问题的贪心算法
  *
@@ -10,7 +10,7 @@
  *                 MST - 最小生成树
  */
 
-const grapg = [
+const graph = [
   [0, 2, 4, 0, 0, 0],
   [2, 0, 2, 4, 2, 0],
   [4, 2, 0, 0, 3, 0],
@@ -37,25 +37,33 @@ const prim = (graph) => {
   // 根节点的父节点
   parent[0] = -1
   for (let i = 0; i < length - 1; i++) {
+    // 找出最小值
     const u = minKey(graph, key, visited)
     visited[u] = true
     for (let v = 0; v < length; v++) {
       if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) {
+        // 如果有更小的权值，则保存MST路径
         parent[v] = u
+        // 然后更新权值
         key[v] = graph[u][v]
       }
     }
   }
+  // 返回包含MST的结果
+  return parent
 }
 
 const minKey = (graph, key, visited) => {
-  // let min = INF
-  // let minIndex = -1
-  // for (let v = 0; v < dist.length; v++) {
-  //   if (visited[v] === false && dist[v] <= min) {
-  //     min = dist[v]
-  //     minIndex = v
-  //   }
-  // }
-  // return minIndex
+  let min = INF
+  let minIndex = 0
+  for (let v = 0; v < graph.length; v++) {
+    if (visited[v] === false && key[v] < min) {
+      min = key[v]
+      minIndex = v
+    }
+  }
+  return minIndex
 }
+
+const res =  prim(graph)
+console.log(res);
