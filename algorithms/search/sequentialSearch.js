d2accd930cbd77a477808ad23239df0dd3460c65
gu_th <1842987765@qq.com>
2023-01-16 17:00:20 +0800

    add： 顺序搜索实现

---
+++ algorithms/search/sequentialSearch.js
@@ -0,0 +1,19 @@
+/*
+ * @Author       : guth
+ * @Date         : 2023-01-16 15:08:59
+ * @LastEditors  : guth
+ * @LastEditTime : 2023-01-16 15:10:54
+ * @FilePath     : /study-javascript-data-structures/algorithms/search/sequentialSearch.js
+ * @Description  : 顺序搜索 （效率比较低）
+ */
+
+const { defaultEquals } = require("../../utils/utils");
+
+function sequentialSearch (arr, value, equalsFn = defaultEquals) {
+  for (let i = 0; i < arr.length; i++) {
+    if (equalsFn(value, arr[i])) {
+      return i
+    }
+  }
+  return -1
+}
