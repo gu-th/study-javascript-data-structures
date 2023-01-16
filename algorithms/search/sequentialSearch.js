ea6a27e95f0bac671c3b44f9af4ad25c5848b914
gu_th <1842987765@qq.com>
2023-01-16 17:04:03 +0800

    update： 顺序搜索实现

--- algorithms/search/sequentialSearch.js
+++ algorithms/search/sequentialSearch.js
@@ -2,11 +2,12 @@
  * @Author       : guth
  * @Date         : 2023-01-16 15:08:59
  * @LastEditors  : guth
- * @LastEditTime : 2023-01-16 15:10:54
+ * @LastEditTime : 2023-01-16 17:02:04
  * @FilePath     : /study-javascript-data-structures/algorithms/search/sequentialSearch.js
  * @Description  : 顺序搜索 （效率比较低）
  */
 
+const { DOES_NOT_EXIST } = require("../../utils/constant");
 const { defaultEquals } = require("../../utils/utils");
 
 function sequentialSearch (arr, value, equalsFn = defaultEquals) {
@@ -15,5 +16,5 @@ function sequentialSearch (arr, value, equalsFn = defaultEquals) {
       return i
     }
   }
-  return -1
+  return DOES_NOT_EXIST
 }
